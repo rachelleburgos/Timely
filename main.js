824cfd37cee063f1ad62ffd76852b3ba291fa01d
Linus Trahair <linus.trahair@gmail.com>
2023-10-04 17:43:58 -0400

    added electron support will open when electron starts

--- main.js
+++ main.js
@@ -20,11 +20,31 @@ async function startserver(){
         await client.close();
         break;
     }
+    return "hello World";
 }
 
 }
 async function run(client){
     console.log("correct");
+    return "hello World";
 }
 
-startserver();
+
+const {app,BrowserWindow}=require('electron');
+const createWindow=()=>{
+    const win =new BrowserWindow({width:400,height:400});
+    win.loadFile("index.html");
+}
+app.whenReady().then(()=>{
+    createWindow();
+    startserver();
+    app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })

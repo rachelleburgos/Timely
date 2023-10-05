0beca00b9c8ea72dd5776aa3fb6ff02637df3174
Linus Trahair <linus.trahair@gmail.com>
2023-10-05 11:52:35 -0400

    fixed network errors

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const {MongoClient} = require('mongodb');
 
 async function startserver(){
     while(true){
-    const username="";
-    const password="";
-    const cluster="";
+    const username="linustrahair";
+    const password="knuckles";
+    const cluster="cluster0.5xyhlnf.mongodb.net";
     const uri =`mongodb+srv://${username}:${password}@${cluster}/test?retryWrites=true&w=majority`;
     try{const client=new MongoClient(uri);
     await client.connect();
@@ -14,7 +14,6 @@ async function startserver(){
         console.log("\n incorrect settings re-enter");
     }
     finally{
-        await client.close();
         break;
     }
     return "hello World";

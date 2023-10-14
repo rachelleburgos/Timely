1c9c688370dac1931dd70da09503e0079107dd43
Rachelle Burgos <rachelle.burgoss@gmail.com>
2023-10-14 17:25:27 -0400

    Fix the file structure...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,11 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-
-// Remove Preload scripts loading
-postMessage({ payload: 'removeLoading' }, '*')
-
-// Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})

1936419af7da4a5a9376585768b6bf1f8778674f
Rachelle Burgos <rachelle.burgoss@gmail.com>
2023-10-15 12:47:34 -0400

    post routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,25 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mern';
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));

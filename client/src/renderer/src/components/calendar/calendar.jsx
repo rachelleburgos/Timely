b96148cdddb1515150f7cd15673f0a322034b74f
Rachelle Burgos <rachelle.burgoss@gmail.com>
2023-11-02 10:05:18 -0400

    added views

--- client/src/renderer/src/components/calendar/calendar.jsx
+++ client/src/renderer/src/components/calendar/calendar.jsx
@@ -1,10 +1,42 @@
 import FullCalendar from '@fullcalendar/react'
 import daygridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 export const MyCalendar = () => {
+  const [events, setEvents] = useState([])
+
+  const handleSelect = (info) => {
+    const { start, end } = info
+    const eventNamePrompt = prompt('Enter, event name')
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title: eventNamePrompt,
+          id: uuid()
+        }
+      ])
+    }
+  }
+
   return (
     <div>
-      <FullCalendar plugins={[daygridPlugin]} />
+      <FullCalendar
+        editable
+        selectable
+        events={events}
+        select={handleSelect}
+        headerToolbar={{
+          start: 'today prev next',
+          end: 'dayGridMonth dayGridWeek dayGridDay'
+        }}
+        plugins={[daygridPlugin, interactionPlugin]}
+        views={['dayGridMonth', 'dayGridWeek', 'dayGridDay']}
+      />
     </div>
   )
 }

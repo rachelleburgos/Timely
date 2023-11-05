46fdb5b3b7336ec00bd89c5907570cfd676cd300
WRahman02 <wahidur.rahman62@myhunter.cuny.edu>
2023-11-05 13:55:24 -0800

    help

---
+++ ml/src/endpoint/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const brain = require('brain.js');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+const trainedModel = JSON.parse(fs.readFileSync('../trained.json', 'utf-8'));
+const net = new brain.NeuralNetwork().fromJSON(trainedModel);
+
+app.use(express.json());
+
+app.post('/predict', (req, res) => {
+    try {
+        const inputData = req.body;
+
+        const predictions = net.run(inputData);
+
+        res.json({predictions});
+    } catch(error) {
+        res.status(500).json({ error: 'Prediction failed.' });
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`server is running on ${port}`);
+})

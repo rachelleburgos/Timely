e9e57172193520d89d585ae7f2ea1119e1d2010b
Linus Trahair <linus.trahair@gmail.com>
2023-12-06 14:54:45 -0500

    added first version of loading element, not finalized

---
+++ client/src/renderer/src/components/SplashLogo/SplashLogo.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Splash.css'
+function SplashLogo(props){
+const [beginAnimation,setAnimation]=useState(false);
+const  animationqueue="";
+return (<div className={`clockElement ${props.isVisible} ${props.fadeout}`}>
+<div className={`hammerleft ${props.fadeout}`} ></div>
+<div className={`hammerright ${props.fadeout}`}></div>
+<div className={`clock ${props.disappear}`}>
+  <div className={`hour ${props.hourAnimation} ${props.fadeout}`}></div>
+<div className={`minute ${props.minuteAnimation} ${props.fadeout}`}></div>
+</div>
+</div>
+)
+}
+export default SplashLogo;

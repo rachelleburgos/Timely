76bf5445f6a37c5f58a9c35139f791da228ef231
Linus Trahair <linus.trahair@gmail.com>
2023-12-06 21:45:15 -0500

    fixed issues and errors

--- client/src/renderer/src/components/SplashLogo/Example.jsx
+++ client/src/renderer/src/components/SplashLogo/Example.jsx
@@ -1,7 +1,6 @@
 import SplashLogo from './SplashLogo.jsx'
 import { useState } from 'react'
 
-<<<<<<< HEAD
 function Example(){
     const [loaddisplay,setDisplay] =useState("notAppear")
   const [fade_out,setFadeOutAnimation]=useState("")
@@ -13,19 +12,6 @@ function Example(){
     setMinuteAnimation("")
     setHourAnimation("")
     setInterval(loadingAnimation, 0.003);
-=======
-function Example() {
-  const [loaddisplay, setDisplay] = useState('notAppear')
-  const [fade_out, setFadeOutAnimation] = useState('')
-  const [minuteAnimation, setMinuteAnimation] = useState('')
-  const [hourAnimation, setHourAnimation] = useState('')
-  const animate = () => {
-    setDisplay('notAppear')
-    setDisplayAnimation('')
-    setMinuteAnimation('')
-    setHourAnimation('')
-    setInterval(loadingAnimation, 0.003)
->>>>>>> 9bc2a3f0bf882ca2aee9f65b2e35a2d69c514bdd
   }
   const loadingAnimation = () => {
     setDisplay('')
--- client/src/renderer/src/components/SplashLogo/SplashLogo.jsx
+++ client/src/renderer/src/components/SplashLogo/SplashLogo.jsx
@@ -2,10 +2,7 @@ import { useState } from 'react'
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Splash.css'
-<<<<<<< HEAD
 function SplashLogo(props){
-const [beginAnimation,setAnimation]=useState(false);
-const  animationqueue="";
 return (<div className={`clockElement ${props.isVisible} ${props.fadeout}`}>
 <div className={`hammerleft ${props.fadeout}`} ></div>
 <div className={`hammerright ${props.fadeout}`}></div>
@@ -23,20 +20,3 @@ SplashLogo.propTypes={
   minuteAnimation: PropTypes.string
 }
 export default SplashLogo;
-=======
-function SplashLogo(props) {
-  const [beginAnimation, setAnimation] = useState(false)
-  const animationqueue = ''
-  return (
-    <div className={`clockElement ${props.isVisible} ${props.fadeout}`}>
-      <div className={`hammerleft ${props.fadeout}`}></div>
-      <div className={`hammerright ${props.fadeout}`}></div>
-      <div className={`clock ${props.disappear}`}>
-        <div className={`hour ${props.hourAnimation} ${props.fadeout}`}></div>
-        <div className={`minute ${props.minuteAnimation} ${props.fadeout}`}></div>
-      </div>
-    </div>
-  )
-}
-export default SplashLogo
->>>>>>> 9bc2a3f0bf882ca2aee9f65b2e35a2d69c514bdd
